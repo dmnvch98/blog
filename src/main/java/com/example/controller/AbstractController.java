d93840832949f35b3f163c8cbbe099a94134e63d
dmn98 <wildehaifisch@gmail.com>
2020-12-27 22:05:48 +0300

    6 lesson

---
+++ src/main/java/com/example/controller/AbstractController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+
+public abstract class AbstractController extends HttpServlet{
+    private static final long serialVersionUID = 6995593219882488672L;
+    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    public final void forwardToPage(String jspPage, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("currentPage", "page/" + jspPage);
+        req.getRequestDispatcher("/WEB-INF/JSP/page-template.jsp").forward(req,resp);
+    }
+
+    public final void forwardToFragment(String jspPage, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/JSP/fragment" + jspPage).forward(req,resp);
+    }
+
+}
---
+++ src/main/java/com/example/controller/page/NewsController.java
@@ -0,0 +1,22 @@
+package com.example.controller.page;
+
+import com.example.controller.AbstractController;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet({"/news", "/news/*"})
+public class NewsController extends AbstractController {
+    private static final long serialVersionUID = 216595477139640552L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          forwardToPage("news.jsp", req, resp);
+
+    }
+}
