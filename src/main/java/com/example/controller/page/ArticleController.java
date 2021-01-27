ba6dcc112a0d23023632b484a54dc7286fb2272e
dmn98 <wildehaifisch@gmail.com>
2021-01-27 21:38:16 +0300

    7 lesson

---
+++ src/main/java/com/example/controller/page/ArticleController.java
@@ -0,0 +1,20 @@
+package com.example.controller.page;
+
+import com.example.controller.AbstractController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/article")
+public class ArticleController extends AbstractController {
+
+    private static final long serialVersionUID = 6095514424135169854L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        forwardToPage("article.jsp", req, resp);
+    }
+}
---
+++ src/main/java/com/example/controller/page/SearchController.java
@@ -0,0 +1,20 @@
+package com.example.controller.page;
+
+import com.example.controller.AbstractController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/search")
+public class SearchController extends AbstractController {
+
+    private static final long serialVersionUID = 6095514424135169854L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        forwardToPage("search.jsp", req, resp);
+    }
+}
